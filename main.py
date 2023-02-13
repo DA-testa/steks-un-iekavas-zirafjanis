e23eade88155c5aa892e2051785bb493a2fae1da
Jānis Bērziņš <125191092+zirafjanis@users.noreply.github.com>
2023-02-13 13:02:29 +0000

    sveiki

--- main.py
+++ main.py
@@ -13,19 +13,26 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i))
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-print("hello")
-#adjfodnfojsdjof
-#rgtgtthyhyjhhjkn
+            if not opening_brackets_stack:
+                return i + 1
+            if not are_matching(opening_brackets_stack[-1].char, next):
+                return i + 1
+            opening_brackets_stack.pop()
+
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position + 1
+    return "Success"
+
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    if isinstance(mismatch, int):
+        print(f"{mismatch}")
+    else:
+        print(mismatch)
 
 
 if __name__ == "__main__":
