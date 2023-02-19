a35a69c560be9b8cd81e83d470f7c3707f60b037
Jānis Bērziņš <125191092+zirafjanis@users.noreply.github.com>
2023-02-19 21:56:13 +0200

    update3

--- main.py
+++ main.py
@@ -16,14 +16,13 @@ def find_mismatch(text):
             opening_brackets_stack.append(Bracket(next, i))
 
         if next in ")]}":
-            if not opening_brackets_stack:
-                return i + 1
-            if not are_matching(opening_brackets_stack[-1].char, next):
-                return i + 1
-            opening_brackets_stack.pop()
+            if not opening_brackets_stack or not are_matching(
+          opening_brackets_stack[-1].char, next):
+        return i + 1
+      opening_brackets_stack.pop()
 
-    if opening_brackets_stack:
-        return opening_brackets_stack[0].position + 1
+  if opening_brackets_stack:
+    return opening_brackets_stack[0].position
     return "Success"
 
 def main():
