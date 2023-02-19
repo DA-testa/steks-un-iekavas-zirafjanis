c114e3a801a15318e1cfcaea587cca08dba3d93b
Jānis Bērziņš <125191092+zirafjanis@users.noreply.github.com>
2023-02-19 21:57:24 +0200

    update5
    es neko nesaprotu

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ def find_mismatch(text):
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(
           opening_brackets_stack[-1].char, next):
-        return i + 1
-      opening_brackets_stack.pop()
+                return i + 1
+    opening_brackets_stack.pop()
 
-  if opening_brackets_stack:
-    return opening_brackets_stack[0].position
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
     return "Success"
 
 def main():
