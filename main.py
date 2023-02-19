d2f5fcf810bc8613b4e8e9431b0fc086fd0840bc
Jānis Bērziņš <125191092+zirafjanis@users.noreply.github.com>
2023-02-19 22:06:03 +0200

    iahdfishdfjs

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i))
-
-        if next in ")]}":
+            pass
+    if next in ")]}":
             if not opening_brackets_stack:
                 return i + 1
-            if not are_matching(opening_brackets_stack[-1].char, next):
+            last_open = opening_brackets_stack.pop()
+            if not are_matching(last_open.char, next):
                 return i + 1
-            opening_brackets_stack.pop()
 
     if opening_brackets_stack:
         return opening_brackets_stack[0].position + 1
